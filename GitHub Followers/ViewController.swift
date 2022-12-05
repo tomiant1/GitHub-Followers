f088938d09fe4af129806beac80b5b4ea8869e0e
tomiant1 <tomi@joinhangoo.com>
2022-12-05 10:25:47 +0100

    Initial Commit

---
+++ GitHub Followers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHub Followers
+//
+//  Created by Tomi Antoljak on 12/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
